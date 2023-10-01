29a458bb4ad18201daa17fd8493999e52c2209dd
Muhammad51264 <muh.freijat@gmail.com>
2023-10-01 16:45:26 +0300

    changed NO_ITEMS_MESSAGE to NO_ITEMS_ERROR

--- src/components/movie/NoItems.jsx
+++ src/components/movie/NoItems.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import { NoItemDiv } from "components/movie/movieStyles";
-import { NO_ITEMS_MESSAGE } from "data/constants";
+import { NO_ITEMS_ERROR } from "data/constants";
 
 /**
  * MoviesContainer component renders when there is no movie to display.
  *
  */
 function NoItems() {
-  return <NoItemDiv>{NO_ITEMS_MESSAGE}</NoItemDiv>;
+  return <NoItemDiv>{NO_ITEMS_ERROR}</NoItemDiv>;
 }
 
 export default NoItems;
--- src/data/constants.js
+++ src/data/constants.js
@@ -1,3 +1,3 @@
 export const MOVIE_PAGE_MAIN_TITLE = "Popular Movies";
 
-export const NO_ITEMS_MESSAGE = "No items were found that match your query.";
+export const NO_ITEMS_ERROR = "No items were found that match your query.";
